f8108a7a6a1cecf90756c7f19ca45097c358a4af
Yash Sangha <89467@farnborough.ac.uk>
2019-11-22 15:05:48 +0000

    Started Phase 3

---
+++ resources/client/js/index.js
@@ -0,0 +1,15 @@
+function pageLoad() {
+
+    let now = new Date();
+
+    let myHTML = '<div style="text-align:center;">'
+        + '<h1>Welcome to my API powered website!</h1>'
+        + '<img src="/client/img/download.jpg"  alt="Logo"/>'
+        + '<div style="font-style: italic;">'
+        + 'Generated at ' + now.toLocaleTimeString()
+        + '</div>'
+        + '</div>';
+
+    document.getElementById("testDiv").innerHTML = myHTML;
+
+}
