540cb0da62f0693bd31584546cfb255dbfed0e52
Yash Sangha <89467@farnborough.ac.uk>
2020-01-16 23:05:27 +0000

    Added sign up and changed design

--- resources/client/js/login.js
+++ resources/client/js/login.js
@@ -1,12 +1,27 @@
 function pageLoad() {
 
     if(window.location.search === '?logout') {
-        document.getElementById('content').innerHTML = '<h1>Logging out, please wait...</h1>';
+        document.getElementById('containment').innerHTML = '<h1>Logging out, please wait...</h1>';
         logout();
     } else {
-        document.getElementById("loginButton").addEventListener("click", login);
-    }
+        const loginButton = document.getElementById("loginButton");
+        const registerButton = document.getElementById("registerButton");
+        const signUpButton = document.getElementById('signUp');
+        const signInButton = document.getElementById('signIn');
+        const containment = document.getElementById('containment');
 
+
+        loginButton.addEventListener("click", login);
+        registerButton.addEventListener("click", register);
+
+        signInButton.addEventListener('click', () => {
+            containment.classList.remove("right-panel-active");
+        });
+
+        signUpButton.addEventListener('click', () => {
+            containment.classList.add("right-panel-active");
+        });
+    }
 }
 
 function login(event) {
@@ -50,4 +65,29 @@ function logout() {
         }
     });
 
+}
+
+function register(event) {
+
+    event.preventDefault()
+
+    const form = document.getElementById("registerForm");
+    const formData = new FormData(form);
+
+    fetch("/user/new",{method: 'post', body: formData}
+    ).then(response => response.json()
+    ).then(responseData => {
+
+        if (responseData.hasOwnProperty('error')) {
+
+            alert(responseData.error);
+
+        } else {
+
+            alert("Register Complete! Login to continue!")
+
+            window.location.href = '../login.html';
+
+        }
+    });
 }
